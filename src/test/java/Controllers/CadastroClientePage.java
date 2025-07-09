35cadb8c495ca67d997b999a77dd2b40eb142bb6
Mauricio <mneto@id.uff.br>
2025-07-09 00:24:28 -0300

    Criação do teste de sistema de cadastro de novo usuário

---
+++ src/test/java/Controllers/CadastroClientePage.java
@@ -0,0 +1,60 @@
+package Controllers; 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select; 
+
+public class CadastroClientePage {
+
+    private WebDriver driver; 
+    private String url = "http://localhost:8080/view/cadastro/cadastro.html"; 
+
+
+    By nomeInput = By.id("Espaçamentotitle"); 
+    By sobrenomeInput = By.name("sobrenome"); 
+    By telefoneInput = By.name("telefone");
+    By usuarioInput = By.name("usuario"); 
+    By senhaInput = By.name("senha"); 
+
+    By ruaInput = By.name("rua");
+    By numeroInput = By.name("numero");
+    By bairroInput = By.name("bairro"); 
+    By complementoInput = By.name("complemento");
+    By cidadeInput = By.name("cidade"); 
+    By estadoSelect = By.id("UF"); 
+
+    By cadastrarButton = By.cssSelector("button.buttonSubmit"); 
+
+    
+    public CadastroClientePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+    public void navigateTo() {
+        driver.get(url);
+    }
+
+    public void preencherDadosUsuario(String nome, String sobrenome, String telefone, String usuario, String senha) {
+        driver.findElement(nomeInput).sendKeys(nome);
+        driver.findElement(sobrenomeInput).sendKeys(sobrenome);
+        driver.findElement(telefoneInput).sendKeys(telefone);
+        driver.findElement(usuarioInput).sendKeys(usuario);
+        driver.findElement(senhaInput).sendKeys(senha);
+    }
+
+    public void preencherDadosEndereco(String rua, String numero, String bairro, String complemento, String cidade, String estado) {
+        driver.findElement(ruaInput).sendKeys(rua);
+        driver.findElement(numeroInput).sendKeys(numero);
+        driver.findElement(bairroInput).sendKeys(bairro);
+        driver.findElement(complementoInput).sendKeys(complemento);
+        driver.findElement(cidadeInput).sendKeys(cidade);
+        
+        Select estadoDropdown = new Select(driver.findElement(estadoSelect));
+        estadoDropdown.selectByValue(estado);
+    }
+
+    public void clicarCadastrar() {
+        driver.findElement(cadastrarButton).click();
+    }
+}
---
+++ src/test/java/Controllers/LoginPage.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private WebDriver driver;
+    private String url = "http://localhost:8080/view/login/login.html"; 
+
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getUrlAtual() {
+        return driver.getCurrentUrl();
+    }
+
+    public boolean taNoLogin() {
+        return getUrlAtual().equals(url);
+    }
+}
